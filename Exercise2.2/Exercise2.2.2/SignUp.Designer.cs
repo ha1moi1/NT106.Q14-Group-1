43b29a28da57aff66acb2ae9d68cd033abd19804
ha1moi1 <24520435@gm.uit.edu.vn>
2025-10-01 22:19:49 +0700

    Fixed UseSystemPasswordChar to True

--- Exercise2.2/Exercise2.2.2/SignUp.Designer.cs
+++ Exercise2.2/Exercise2.2.2/SignUp.Designer.cs
@@ -51,7 +51,7 @@
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
             pictureBox1.Location = new Point(0, 0);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(1262, 712);
+            pictureBox1.Size = new Size(1262, 676);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
@@ -59,10 +59,10 @@
             // 
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Microsoft Sans Serif", 15.7090893F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(346, 99);
+            label1.Location = new Point(346, 94);
             label1.Name = "label1";
-            label1.Padding = new Padding(20, 21, 20, 21);
-            label1.Size = new Size(520, 512);
+            label1.Padding = new Padding(20, 20, 20, 20);
+            label1.Size = new Size(520, 486);
             label1.TabIndex = 1;
             label1.Text = "ĐĂNG KÝ";
             label1.TextAlign = ContentAlignment.TopCenter;
@@ -70,43 +70,43 @@
             // textBox1
             // 
             textBox1.Font = new Font("Microsoft Sans Serif", 13.090909F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox1.Location = new Point(386, 197);
+            textBox1.Location = new Point(386, 187);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(452, 32);
+            textBox1.Size = new Size(452, 30);
             textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
             textBox2.Font = new Font("Microsoft Sans Serif", 13.090909F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox2.Location = new Point(386, 271);
+            textBox2.Location = new Point(386, 257);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(452, 32);
+            textBox2.Size = new Size(452, 30);
             textBox2.TabIndex = 3;
-
             // 
             // textBox3
             // 
             textBox3.Font = new Font("Microsoft Sans Serif", 13.090909F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox3.Location = new Point(386, 349);
+            textBox3.Location = new Point(386, 332);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(452, 32);
+            textBox3.Size = new Size(452, 30);
             textBox3.TabIndex = 4;
-
+            textBox3.UseSystemPasswordChar = true;
             // 
             // textBox4
             // 
             textBox4.Font = new Font("Microsoft Sans Serif", 13.090909F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox4.Location = new Point(386, 432);
+            textBox4.Location = new Point(386, 410);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(452, 32);
+            textBox4.Size = new Size(452, 30);
             textBox4.TabIndex = 5;
+            textBox4.UseSystemPasswordChar = true;
             // 
             // btnDangKi
             // 
             btnDangKi.Font = new Font("Microsoft Sans Serif", 13.090909F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnDangKi.Location = new Point(541, 505);
+            btnDangKi.Location = new Point(541, 480);
             btnDangKi.Name = "btnDangKi";
-            btnDangKi.Size = new Size(129, 42);
+            btnDangKi.Size = new Size(129, 40);
             btnDangKi.TabIndex = 6;
             btnDangKi.Text = "Đăng ký";
             btnDangKi.UseVisualStyleBackColor = true;
@@ -117,19 +117,20 @@
             label2.AutoSize = true;
             label2.BackColor = Color.Transparent;
             label2.Font = new Font("Microsoft Sans Serif", 11.1272726F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(606, 551);
+            label2.Location = new Point(621, 533);
             label2.Name = "label2";
-            label2.Size = new Size(149, 24);
+            label2.Size = new Size(134, 20);
             label2.TabIndex = 7;
             label2.Text = "Đã có tài khoản?";
+            label2.Click += label2_Click;
             // 
             // llblDangNhap
             // 
             llblDangNhap.AutoSize = true;
             llblDangNhap.Font = new Font("Microsoft Sans Serif", 11.1272726F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            llblDangNhap.Location = new Point(750, 551);
+            llblDangNhap.Location = new Point(749, 533);
             llblDangNhap.Name = "llblDangNhap";
-            llblDangNhap.Size = new Size(103, 24);
+            llblDangNhap.Size = new Size(89, 20);
             llblDangNhap.TabIndex = 8;
             llblDangNhap.TabStop = true;
             llblDangNhap.Text = "Đăng nhập";
@@ -139,9 +140,9 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
-            label3.Location = new Point(386, 174);
+            label3.Location = new Point(386, 165);
             label3.Name = "label3";
-            label3.Size = new Size(137, 23);
+            label3.Size = new Size(131, 21);
             label3.TabIndex = 9;
             label3.Text = "Tên người dùng";
             // 
@@ -149,9 +150,9 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
-            label4.Location = new Point(386, 248);
+            label4.Location = new Point(386, 236);
             label4.Name = "label4";
-            label4.Size = new Size(54, 23);
+            label4.Size = new Size(53, 21);
             label4.TabIndex = 10;
             label4.Text = "Email";
             // 
@@ -159,9 +160,9 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
-            label5.Location = new Point(386, 326);
+            label5.Location = new Point(386, 310);
             label5.Name = "label5";
-            label5.Size = new Size(86, 23);
+            label5.Size = new Size(82, 21);
             label5.TabIndex = 11;
             label5.Text = "Mật khẩu";
             // 
@@ -169,17 +170,18 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
-            label6.Location = new Point(386, 409);
+            label6.Location = new Point(386, 389);
             label6.Name = "label6";
-            label6.Size = new Size(155, 23);
+            label6.Size = new Size(151, 21);
             label6.TabIndex = 12;
-            label6.Text = "nhập lại mật khẩu";
+            label6.Text = "Nhập lại mật khẩu";
+            label6.Click += label6_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(8F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1262, 712);
+            ClientSize = new Size(1262, 676);
             Controls.Add(label6);
             Controls.Add(label5);
             Controls.Add(label4);
