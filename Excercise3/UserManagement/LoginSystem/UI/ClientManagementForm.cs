52b2746094771b055aced5c45e24b0b79f83b5cf
LightKer23 <hauvq0468@gmail.com>
2025-10-25 12:29:39 +0700

    feat(ui): add ClientManagementForm layout design

---
+++ Excercise3/UserManagement/LoginSystem/UI/ClientManagementForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginSystem.UI
+{
+    public partial class ClientManagementForm : Form
+    {
+        public ClientManagementForm()
+        {
+            InitializeComponent();
+            this.dataGridView1.Rows.Add(false, "anlv", "255.255.255.255", "80");
+            this.dataGridView1.Rows.Add(false, "binhnv", "192.168.2.5", "53");
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
